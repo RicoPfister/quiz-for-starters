36c7ff50c1929bd4141d98f25fd13712579399ee
Rico Pfister <contact.rico.pfister@gmail.com>
2022-03-29 15:51:03 +0200

    collecting data erweitert

--- questions/question001.php
+++ questions/question001.php
@@ -4,24 +4,24 @@
 
 <!-- Antwortform -->
 
-<form class="ps-5 fs-4" action="index.php" methode="GET">
+<form class="ps-5 fs-4" action="quiz.php" method="GET">
 
 <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio1" name="1" value="1">Lamm
+    <input type="radio" class="form-check-input" name="q1" id="1-1" value="0">Lamm
     <label class="form-check-label" for="radio1"></label>
 </div>
 
 <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio2" name="1" value="2">Hammel
+    <input type="radio" class="form-check-input" name="q1" id="1-2" value="0">Hammel
     <label class="form-check-label" for="radio2"></label>
 </div>
 
 <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio2" name="1" value="3">Widder
+    <input type="radio" class="form-check-input" name="q1" id="1-3"  value="1">Widder
     <label class="form-check-label" for="radio2"></label>
 </div>
 
 <div class="form-check">
-    <input type="radio" class="form-check-input" id="radio2" name="1" value="4">Mufflon
+    <input type="radio" class="form-check-input" name="q1" id="1-4" value="0">Mufflon
     <label class="form-check-label" for="radio2"></label>
 </div>
