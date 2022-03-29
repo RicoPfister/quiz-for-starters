f25ea982729639c7a869f28b23655bd99a7a2126
NinFrei <n.freiman@yahoo.com>
2022-03-29 16:23:26 +0200

    Front-page, drei Seietn Fragen.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sprachschule</title>
+    <title>Naturquiz</title>
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
         integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
@@ -15,12 +15,60 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Cormorant:wght@300;400;500&family=Fredoka:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
+        <style>
+body, html {
+    height: 100%;
+    margin: 0;
+    font-family:'Fredoka';
+}
+   
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/bild_1.jpg");
+  height: 100%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
 
-    <script>
-        window.addEventListener('resize', function () {
-            if (window.innerWidth <= 950) {
-                window.location.href = 'file:///home/oppdev2021/Dokumente/Sprachkurs_1/indexmobile.html';
-            }
-        });
-    </script>
-</head>
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+
+.hero-text button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 10px 25px;
+  color: black;
+  background-color: #ddd;
+  text-align: center;
+  cursor: pointer;
+}
+
+.hero-text button:hover {
+  background-color: #555;
+  color: white;
+}
+
+
+</style>
+</head> 
+<body>
+    <div class="hero-image">  
+    <div class="hero-text">
+    <h1 style="font-size:50px">Quiz: Wunder der Natur</h1>
+    <p class="service-paragraph">     
+    Sie Ihr Wissen über Orte in naher und weiter Ferne. Nehmen Sie an diesem Mul­ti­ple-Choice-Quiz teil und erkunden Sie dabei spielerisch einige der unglaublichsten Wunder der Natur.</p>
+    <button>LOS GEHTS</button>
+
+  </div>
+</div>
+
+</body>
+</html>
