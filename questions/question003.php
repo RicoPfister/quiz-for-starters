cab7d3fad021aadaf339e58d68f849d5beb8a6ed
Rico Pfister <contact.rico.pfister@gmail.com>
2022-03-30 12:30:52 +0200

    test

---
+++ questions/question003.php
@@ -0,0 +1,28 @@
+<!-- Fragetext -->
+
+<h3 class="p-5">Wie nennt man ein männliches Schaf?</h2>
+
+<!-- Antwortform -->
+
+<form class="ps-5 fs-4" action="quiz.php" method="GET" onsubmit="return evaluateCheckboxes();">
+
+<div class="form-check">
+    <input type="radio" class="form-check-input" name="q1" id="1-1" value="0">Lamm
+    <label class="form-check-label" for="radio1"></label>
+</div>
+
+<div class="form-check">
+    <input type="radio" class="form-check-input" name="q1" id="1-2" value="0">Hammel
+    <label class="form-check-label" for="radio2"></label>
+</div>
+
+<div class="form-check">
+    <input type="radio" class="form-check-input" name="q1" id="1-3"  value="1">Widder
+    <label class="form-check-label" for="radio2"></label>
+</div>
+
+<div class="form-check">
+    <input type="radio" class="form-check-input" name="q1" id="1-4" value="0">Mufflon
+    <label class="form-check-label" for="radio2"></label>
+    <p id="validate-warning" class="warning"></p>
+</div>
