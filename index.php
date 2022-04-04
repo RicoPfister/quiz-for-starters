e24ab9e7064f72daa6e624942c3ef626c1dfd296
Rico Pfister <contact.rico.pfister@gmail.com>
2022-04-04 10:21:18 +0200

    commit

--- index.php
+++ index.php
@@ -59,14 +59,16 @@ body, html {
     <h1 class="display-3">Testen Sie Ihr Wissen über Orte in naher und weiter Ferne. Nehmen Sie an diesem Mul­ti­ple-Choice-Quiz teil und erkunden Sie dabei spielerisch einige der unglaublichsten Wunder der Natur.</h1>
     
     <div>
-    <form action="/quiz.php" method="GET">
 
-    <select class="form-select" id="select" name="s">
-      <option selected>Schwierigkeitsgrad</option>
+    <form class="ps-5 fs-4" action="quiz.php" method="GET" id="difficulty" onsubmit="return evaluateDIfficulty()">
+
+    <select class="form-select" id="select" name="s" form="difficulty">
+      <option value="" selected>Schwierigkeitsgrad</option>
       <option value="20">Einfach (20 Sekunden)</option>
       <option value="15">Normal (15 Sekunden)</option>
       <option value="7">Schwierig (7 Sekunden)</option>
     </select>
+    <p id="warning" style="background: white; color: red"></p>
 
     <button style="font-size: 30px" type="submit" class="btn btn-outline btn-lg">Los geht's!</button>
       
@@ -76,6 +78,8 @@ body, html {
   </div>
 </div>
 </div>
+
+<?php include "php/footer.php"?>
 
 </body>
 </html>
--- php/evaluationsSeite.php
+++ php/evaluationsSeite.php
@@ -1,8 +1,11 @@
 <?php include "header.php"?>
 <?php include "evaluation_data.php"?>
 
-
-
+<?php
+echo "<div id='cheat2' style='visibility: hidden; margin-left: 40px; position: absolute'><pre>";
+print_r($_SESSION);
+echo "</pre></div>";
+?>
 
 <style>
 body, html {
@@ -55,9 +58,7 @@ body, html {
   <div class="hero-text" class="font-weight-bold">
     <h1 >RESULTAT</h1>
 
-
-
-    <p class="h2">Sie haben <strong class="text-warning"><?=$points?> Punkt(e)</strong> erzielt.</p>
+    <p class="h2">Du hast auf dem Schwierigkeitsgrad <?=$difficulty?> <strong class="text-warning"><?=$points?> Punkt(e)</strong> erzielt.</p>
 <h2 id="answerText" class="lh-lg"> <?=$text?> </h2>
 
 
